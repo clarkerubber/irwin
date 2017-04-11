3021701eeb9b628b9eaee5fca10f361f91186d49
James Clarke <jamesjosephclarke@gmail.com>
2017-04-12 08:39:07 +1000

    finish laying initial foundations

--- modules/Game.py
+++ modules/Game.py
@@ -1,29 +1,19 @@
 from operator import attrgetter
 from bson.objectid import ObjectId
+from modules.PlayerAssessment import PlayerAssessments
+from collections import namedtuple
 
-class Game:
-  def __init__(self, _id, pgn, emts):
-    self.id = _id
-    self.pgn = pgn
-    self.emts = emts
-
+class Game(namedtuple('Game', ['id', 'pgn', 'emts'])):
   def getEmt(self, ply):
     return self.emts[ply]
-
-  def __str__(self):
-    return str(self.json())
-
-  def json(self):
-    return {'_id': self.id,
-      'pgn': self.pgn,
-      'emts': self.emts}
 
 def gameLength(pgn):
     return len(pgn.split(' '))
 
 def recentGames(playerAssessments, gameJSONs):
   try:
-    playerAssessments.playerAssessments = sorted(playerAssessments.playerAssessments, key = lambda x: (attrgetter('assessment'), attrgetter('date')), reverse=True)
+    playerAssessments = PlayerAssessments(sorted(playerAssessments.playerAssessments,
+      key = lambda x: (attrgetter('assessment'), attrgetter('date')), reverse=True))
     return Games(list(Game(pa.gameId, gameJSONs[pa.gameId]['pgn'], gameJSONs[pa.gameId]['emts']) for pa in playerAssessments.playerAssessments if 
       'variant' not in gameJSONs[pa.gameId] and
       'emts' in gameJSONs[pa.gameId] and
@@ -34,16 +24,7 @@ def recentGames(playerAssessments, gameJSONs):
     return []
 
 # thin wrapper class for multiple games
-class Games:
-  def __init__(self, games):
-    self.games = games # List[Game]
-
-  def __str__(self):
-    return str([str(g) for g in self.games])
-
-  def json(self):
-    return [g.json() for g in self.games]
-
+class Games(namedtuple('Games', ['games'])):
   def byId(self, gameId):
     return next(iter([g for g in self.games if g.id == gameId]), None)
 
@@ -53,9 +34,21 @@ class Games:
   def hasId(self, gameId):
     return (gameId in self.ids())
 
-# For everything database related
-def JSONToGame(json):
-  return Game(json['_id'], json['pgn'], json['emts'])
+class GameBSONHandler:
+  @staticmethod
+  def reads(bson):
+    return Game(
+      id = bson['_id'],
+      pgn = bson['pgn'],
+      emts = bson['emts'])
+
+  @staticmethod
+  def writes(game):
+    return {
+      '_id': game.id,
+      'pgn': game.pgn,
+      'emts': game.emts
+    }
 
 class GameDB:
   def __init__(self, gameColl):
@@ -63,19 +56,19 @@ class GameDB:
 
   def byId(self, _id):
     try:
-      return Game(self.gameColl.find_one({'_id': ObjectId(_id)}))
+      return GameBSONHandler(self.gameColl.find_one({'_id': _id}))
     except:
-      return None # ugly
+      return None
 
   def byIds(self, ids): # List[Ids]
-    return Games(list([JSONToGame(g) for g in self.gameColl.find({'_id': {'$in': list([i for i in ids])}})]))
+    return Games(list([GameBSONHandler.reads(g) for g in self.gameColl.find({'_id': {'$in': list([i for i in ids])}})]))
 
   def write(self, game): # Game
-    self.gameColl.update_one({'_id': game.json()['_id']}, {'$set': game.json()}, upsert=True)
+    self.gameColl.update_one({'_id': game.id}, {'$set': GameBSONHandler.writes(game)}, upsert=True)
 
   def writeGames(self, games): # Games
     if len(games.games) > 0:
-      self.gameColl.insert_many(games.json())
+      self.gameColl.insert_many(list([GameBSONHandler.writes(g) for g in games.games]))
 
   def lazyWriteGames(self, games):
     [self.write(g) for g in games.games]
