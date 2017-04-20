69c71fd62b4ec5a3ee6147fbb995e8dcc1ac0dc2
James Clarke <jamesjosephclarke@gmail.com>
2017-04-21 03:08:30 +1000

    make hella progress on final implementation

--- modules/irwin/Irwin.py
+++ modules/irwin/Irwin.py
@@ -1,18 +1,63 @@
 from collections import namedtuple
 import threading
+import datetime
+import time
 
-from modules.irwin.Train import Train
+from modules.irwin.TrainNetworks import TrainNetworks
+from modules.irwin.MoveAssessment import MoveAssessment
+from modules.irwin.ChunkAssessment import ChunkAssessment
+from modules.core.AssessedMove import AssessedMove
+from modules.core.AssessedChunk import AssessedChunk
+from modules.core.PlayerAnalysis import PlayerAnalysis
 
 class Irwin(namedtuple('Irwin', ['api', 'learner', 'trainingStatsDB', 'playerAnalysisDB'])):
-  def train(self):
+  def train(self): # runs forever
     if self.learner == 1:
-      Train(self.api, self.trainingStatsDB, self.playerAnalysisDB).start()
+      TrainAndEvaluate(self.api, self.trainingStatsDB, self.playerAnalysisDB).start()
 
-  def assessMove(self, gameAnalysis, analysedMove): # Pass move to neural network and assess it.
-    pass
+  @staticmethod
+  def assessGame(gameAnalysis, titled):
+    gameAnalysis.assessedMoves = [AssessedMove(analysedMove, irwinReport) for analysedMove, irwinReport in zip(gameAnalysis.analysedMoves, MoveAssessment.applyNet(gameAnalysis.tensorInputMoves(titled)))]
+    gameAnalysis.assessedChunks = [AssessedChunk(move, irwinReport) for move, irwinReport in enumerate(ChunkAssessment.applyNet(gameAnalysis.tensorInputChunks(titled)))]
+    return gameAnalysis
 
-  def assessGame(self, gameAnalysis):
-    [self.assessMove(gameAnalysis, analysedMove) for analysedMove in gameAnalysis.analysedMoves]
+  @staticmethod
+  def assessPlayer(playerAnalysis):
+    return PlayerAnalysis(
+      id = playerAnalysis.id,
+      titled = playerAnalysis.titled,
+      engine = playerAnalysis.engine,
+      gamesPlayed = playerAnalysis.gamesPlayed,
+      closedReports = playerAnalysis.closedReports,
+      gameAnalyses = [Irwin.assessGame(gameAnalysis, playerAnalysis.titled) for gameAnalysis in playerAnalysis.gameAnalyses.gameAnalyses]
+      )
+    
 
-  def assessPlayer(self, playerAnalysis):
-    [self.assessGame(gameAnalysis) for gameAnalysis in playerAnalysis.gameAnalyses]
+class TrainAndEvaluate(threading.Thread):
+  def __init__(self, api, trainingStatsDB, playerAnalysisDB):
+    threading.Thread.__init__(self)
+    self.api = api
+    self.trainingStatsDB = trainingStatsDB
+    self.playerAnalysisDB = playerAnalysisDB
+
+  def run(self):
+    while True:
+      time.sleep(10)
+      if self.outOfDate():
+        trainer = TrainNetworks(self.api, self.playerAnalysisDB)
+        trainer.start()
+        trainer.join()
+        engines = self.playerAnalysisDB.engines()
+        legits = self.playerAnalysisDB.legits()
+        engines = [Irwin.assessPlayer(engine) for engine in engines]
+        legits = [Irwin.assessPlayer(legit) for legit in legits]
+
+
+  def outOfDate(self):
+    latest = self.trainingStatsDB.latest()
+    if latest is not None:
+      if datetime.datetime.utcnow() - latest.date > datetime.timedelta(days=1): # if it has been over a day since the last training
+        return True
+    else:
+      return True
+    return False
