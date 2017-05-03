055ebc6106ce4b49a89aad7556f694034905a5b4
James Clarke <jamesjosephclarke@gmail.com>
2017-05-03 23:06:22 +1000

    put all the things back to normal

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ logging.getLogger("chess.uci").setLevel(logging.WARNING)
 
 env = Env(settings)
 env.irwin.train()
-"""
+
 while True:
   # Get player data
   userId = env.api.getPlayerId()
@@ -89,4 +89,3 @@ while True:
 
   env.playerAnalysisDB.write(playerAnalysis)
   env.api.postReport(playerAnalysis.report())
-"""
