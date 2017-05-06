34656d06fae47e10bf8b43f6fe2e0ee871e8e1ef
James Clarke <jamesjosephclarke@gmail.com>
2017-05-07 01:26:54 +1000

    remove debug points

--- modules/core/recentGames.py
+++ modules/core/recentGames.py
@@ -8,23 +8,18 @@ def recentGames(playerAssessments, gameJSONs):
       key = lambda x: (x.assessment, x.date), reverse=True))
     gs1 = []
     gs2 = []
-    print(gameJSONs)
     for playerAssessment in playerAssessments.playerAssessments:
       gameJSON = gameJSONs.get(playerAssessment.gameId, None)
       pgn = gameJSON.get('pgn', '')
       if gameJSON is not None and 'variant' not in gameJSON and 'emts' in gameJSON and gameLength(pgn) > 44:
         gs1.append(Game(playerAssessment.gameId, pgn, gameJSON['emts']))
     games1 = Games(gs1[:5])
-    """
+    
     for gameId, gameJSON in gameJSONs.items():
       pgn = gameJSON.get('pgn', '')
       if not games1.hasId(gameId) and 'variant' not in gameJSON and 'emts' in gameJSON and gameLength(pgn) > 44:
         gs2.append(Game(gameId, pgn, gameJSON['emts']))
-    games = gs1 + gs2
-    """
-    games = gs1
-    games = Games(games[:10])
-    return games
+    return Games((gs1 + gs2)[:10])
   except ValueError:
     return Games([])
   except IndexError:
