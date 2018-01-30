eae8bbbf8599a8d21731794afd77f35d2edfad5d
James Clarke <jamesjosephclarke@gmail.com>
2018-01-31 09:19:28 +1100

    change game activation to percentage > 90

---
+++ modules/irwin/GameBasicActivation.py
@@ -0,0 +1,47 @@
+from collections import namedtuple
+
+class GameBasicActivation(namedtuple('GameBasicActivation', ['id', 'gameId', 'userId', 'engine', 'prediction'])):
+    @staticmethod
+    def fromPredictionByIdAndUser(gameId, userId, prediction, engine):
+        return GameBasicActivation(
+            id = gameId + '/' + userId,
+            gameId = gameId,
+            userId = userId,
+            engine = engine,
+            prediction = prediction
+            )
+
+class GameBasicActivationBSONHandler:
+    @staticmethod
+    def reads(bson):
+        return GameBasicActivation(
+            id = bson['_id'],
+            gameId = bson['gameId'],
+            userId = bson['userId'],
+            engine = bson['engine'],
+            prediction = bson['prediction'])
+
+    @staticmethod
+    def writes(gba):
+        return {
+            '_id': gba.id,
+            'gameId': gba.gameId,
+            'userId': gba.userId,
+            'engine': gba.engine,
+            'prediction': gba.prediction
+        }
+
+class GameBasicActivationDB(namedtuple('GameBasicActivationDB', ['gameBasicActivationColl'])):
+    def byUserId(self, userId):
+        return [GameBasicActivationBSONHandler.reads(bson) for bson in self.gameBasicActivationColl.find({'userId': userId})]
+
+    def byEngineAndPrediction(self, engine, prediction):
+        if engine:
+            return [GameBasicActivationBSONHandler.reads(bson) for bson in self.gameBasicActivationColl.find({'engine': engine, 'prediction': {'$gte': prediction}})]
+        return [GameBasicActivationBSONHandler.reads(bson) for bson in self.gameBasicActivationColl.find({'engine': engine, 'prediction': {'$lte': prediction}})]
+
+    def write(self, gba): # Game
+        self.gameBasicActivationColl.update_one({'_id': gba.id}, {'$set': GameBasicActivationBSONHandler.writes(gba)}, upsert=True)
+
+    def lazyWriteMany(self, gbas):
+        [self.write(gba) for gba in gbas]
--- modules/irwin/Irwin.py
+++ modules/irwin/Irwin.py
@@ -33,11 +33,27 @@ class Irwin(Evaluation):
         predictions = [int(100*np.asscalar(p)) for p in self.basicGameModel.model().predict(np.array([t for gid, t in gameTensors]))]
         return list(zip(gameIds, predictions))
 
-    @staticmethod
-    def top30avg(moveActivations):
-        top30 = sorted(moveActivations, reverse=True)[:ceil(0.3*len(moveActivations))]
-        res = int(np.average(top30)) if len(top30) > 0 else 0
-        return res
+    def activation(self, predictions, gamesMoveActivations=None):
+        if gamesMoveActivations is None:
+            gamesMoveActivations = [[int(50*(p[0][0] + p[1][0])) for p in zip(list(prediction[1][0]), list(prediction[2][0]))] for prediction in predictions]
+        gamesPOverX = [Irwin.pOverX(moveActivations, 90) for moveActivations in gamesMoveActivations]
+        gameOverallPredictions = [100*p[0][0] for p in predictions]
+        sortedGameActivations = sorted([int(0.5*(a+o)) for a, o in zip(gamesPOverX, gameOverallPredictions)], reverse=True)
+
+        top30games = sortedGameActivations[:ceil(0.3*len(sortedGameActivations))]
+        top30avgGames = int(np.average(top30games)) if len(top30games) > 0 else 0
+
+        above90 = len([a for a in sortedGameActivations if a > 90])
+        above80 = len([a for a in sortedGameActivations if a > 80])
+
+        if len(gamesPOverX) < 6 or above90 < 3:
+            result = min(90, top30avgGames) # Not enough games to mark
+        elif above80 == 0:
+            result = min(60, top30avgGames) # Not enough games to report
+        else:
+            result = top30avgGames # Normal activation
+
+        return result
 
     def report(self, userId, gameAnalysisStore):
         predictions = self.predictAnalysed(gameAnalysisStore.gameAnalysisTensors())
@@ -48,28 +64,6 @@ class Irwin(Evaluation):
             'games': [Irwin.gameReport(ga, p, ma) for ga, p, ma in zip(gameAnalysisStore.gameAnalyses, predictions, gamesMoveActivations)]
         }
         return report
-
-    def activation(self, predictions, gamesMoveActivations=None):
-        if gamesMoveActivations is None:
-            gamesMoveActivations = [[int(50*(p[0][0] + p[1][0])) for p in zip(list(prediction[1][0]), list(prediction[2][0]))] for prediction in predictions]
-        gamesTop30avg = [Irwin.top30avg(moveActivations) for moveActivations in gamesMoveActivations]
-        gameOverallPredictions = [100*p[0][0] for p in predictions]
-        sortedGameActivations = sorted([int(0.5*(a+o)) for a, o in zip(gamesTop30avg, gameOverallPredictions)], reverse=True)
-
-        top30games = sortedGameActivations[:ceil(0.3*len(sortedGameActivations))]
-        top30avgGames = int(np.average(top30games)) if len(top30games) > 0 else 0
-
-        above90 = len([a for a in sortedGameActivations if a > 90])
-        above80 = len([a for a in sortedGameActivations if a > 80])
-
-        if len(gamesTop30avg) < 6 or above90 < 3:
-            result = min(90, top30avgGames) # Not enough games to mark
-        elif above80 == 0:
-            result = min(60, top30avgGames) # Not enough games to report
-        else:
-            result = top30avgGames # Normal activation
-
-        return result
 
     @staticmethod
     def gameReport(gameAnalysis, prediction, moveActivations):
@@ -89,6 +83,19 @@ class Irwin(Evaluation):
             'l': int(100*analysedMove.winningChancesLoss())
         }
 
+    @staticmethod
+    def pOverX(moveActivations, x): # percentage of moveActivations > X in the list
+        l = len([a for a in moveActivations if a > x])
+        if l == 0 or len(moveActivations) == 0:
+            return 0
+        return int(100*l/len(moveActivations))
+
+    @staticmethod
+    def top30avg(moveActivations):
+        top30 = sorted(moveActivations, reverse=True)[:ceil(0.3*len(moveActivations))]
+        res = int(np.average(top30)) if len(top30) > 0 else 0
+        return res
+
     def discover(self):
         # discover potential cheaters in the database of un-marked players
         logging.warning("Discovering unprocessed players")
@@ -106,8 +113,8 @@ class Irwin(Evaluation):
                 sus.append((player.id, activation))
         pprint(sus)
 
-    def buildActivationTable(self):
-        logging.warning("Building Confidence Table")
+    def buildAnalysedActivationTable(self):
+        logging.warning("Building Analysed Activation Table")
         logging.debug("getting games")
         cheats = self.env.playerDB.byEngine(True)
         legits = self.env.playerDB.byEngine(False)
@@ -130,3 +137,25 @@ class Irwin(Evaluation):
 
         logging.debug("writing to db")
         self.env.gameAnalysisActivationDB.lazyWriteMany(confidentCheats + confidentLegits)
+
+    def buildBasicActivationTable(self):
+        logging.warning("Building Basic Activation Table")
+        logging.debug("getting games")
+        cheats = self.env.playerDB.byEngine(True)
+        legits = self.env.playerDB.byEngine(False)
+
+        cheatTensorsByIdAndUser = []
+        legitTensorsByIdAndUser = []
+
+        for p in cheats + legits:
+            tensorsByIdAndUser = [(g.id, p.id, g.tensor(p.id)) for g in self.env.gameDB.byUserId(p.id)]
+            if p.engine:
+                cheatTensorsByIdAndUser.extend(tensorsByIdAndUser)
+            else:
+                legitTensorsByIdAndUser.extend(tensorsByIdAndUser)
+
+        cheatGamePredictions = self.predictBasicGames([(g[0], g[2]) for g in cheatTensorsByIdAndUser])
+        legitGamePredictions = self.predictBasicGames([(g[0], g[2]) for g in legitTensorsByIdAndUser])
+
+        cheatPredictionsByIdAndUser = [(p[0], t[1], p[1]) for p, t in zip(cheatGamePredictions, cheatTensorsByIdAndUser)] # [(gameId, userId, prediction)]
+        legitPredictionsByIdAndUser = [(p[0], t[1], p[1]) for p, t in zip(legitGamePredictions, legitTensorsByIdAndUser)] # [(gameId, userId, prediction)]
