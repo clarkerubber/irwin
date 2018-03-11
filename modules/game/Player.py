559d7d6968bf6bdd1b7994b3f82687d67b4ab2a5
James Clarke <jamesjosephclarke@gmail.com>
2018-03-11 21:56:46 +1100

    fix use of timedelta some more

--- modules/game/Player.py
+++ modules/game/Player.py
@@ -69,7 +69,7 @@ class PlayerDB(namedtuple('PlayerDB', ['playerColl'])):
 
     def oldestNonEngine(self):
         playerBSON = self.playerColl.find_one_and_update(
-            filter={'$or': [{'engine': False}, {'engine': None}], 'date': {'$lt': datetime.now() - timedelta(months=1)}},
+            filter={'$or': [{'engine': False}, {'engine': None}], 'date': {'$lt': datetime.now() - timedelta(days=30)}},
             update={'$set': {'date': datetime.now()}},
             sort=[('date', pymongo.ASCENDING)])
         return None if playerBSON is None else PlayerBSONHandler.reads(playerBSON)
