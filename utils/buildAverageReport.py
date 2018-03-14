3ce0b23466bde9e55bbda8707c56a4c05bb8045c
James Clarke <jamesjosephclarke@gmail.com>
2018-03-15 09:19:22 +1100

    more tracking in averages calculation

--- utils/buildAverageReport.py
+++ utils/buildAverageReport.py
@@ -5,8 +5,10 @@ from pprint import pprint
 from modules.irwin.AnalysisReport import GameReportStore
 
 def gameReportStoreByPlayers(env, players):
+    print('getting player reports against players')
     playerReports = [env.playerReportDB.newestByUserId(player.id) for player in players]
     gameReports = []
+    print('getting game reports against player reports')
     [gameReports.extend(env.gameReportDB.byReportId(report.id)) for report in playerReports if report is not None]
     return GameReportStore(gameReports)
 
@@ -22,19 +24,19 @@ def buildAverageReport(env):
     legitPlayers = env.playerDB.byEngine(False)
     titledPlayers = [player for player in legitPlayers if player.titled]
 
-    print('calculating legit averages')
+    print('---calculating legit averages---')
     legitReportStore = gameReportStoreByPlayers(env, legitPlayers)
     legitAvgs = getAverages(legitReportStore)
     del legitReportStore
     del legitPlayers
 
-    print('calculating titled averages')
+    print('---calculating titled averages---')
     titledReportStore = gameReportStoreByPlayers(env, titledPlayers)
     titledAvgs = getAverages(titledReportStore)
     del titledReportStore
     del titledPlayers
 
-    print('calculating engine averages')
+    print('---calculating engine averages---')
     engineReportStore = gameReportStoreByPlayers(env, env.playerDB.byEngine(True))
     engineAvgs = getAverages(engineReportStore)
     del engineReportStore
