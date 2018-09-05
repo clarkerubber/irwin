5657dbb545f598da52795a045ed0382700975f50
James Clarke <jamesjosephclarke@gmail.com>
2018-09-05 19:56:52 +1000

    don't use ports \o/

--- modules/client/Env.py
+++ modules/client/Env.py
@@ -7,6 +7,6 @@ from modules.game.EngineTools import EngineTools
 class Env:
     def __init__(self, config: ConfigWrapper):
         self.config = config
-        self.url = "{}://{}:{}".format(self.config.server.protocol, self.config.server.domain, self.config.server.port)
+        self.url = "{}://{}".format(self.config.server.protocol, self.config.server.domain)
         self.engineTools = EngineTools.new(self.config)
         self.auth = self.config.auth.asdict()
