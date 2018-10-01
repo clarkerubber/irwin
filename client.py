dcc6d6cd1f00f59b1ca85ed7a809d9560c19af69
Lakin Wecker <lakin@structuredabstraction.com>
2018-10-01 14:37:38 -0600

    Better (and less) logging now that the problem has been identified.

--- client.py
+++ client.py
@@ -50,13 +50,11 @@ def analyseGames(games: List[Game], playerId: str) -> Iterable[AnalysedGame]:
     """
 
     count = len(games)
-    logging.warning(f'{playerId}: Analysing {count} games')
     for i, game in enumerate(games):
-        logging.warning(f'\t{playerId}: Analysing Game #{i}: {game.id}')
+        logging.warning(f'{playerId}: Analysing Game #{i} / {count}: {game.id}')
         analysedGame = env.engineTools.analyseGame(game, game.white == playerId, conf['stockfish nodes'])
         if analysedGame is not None:
             yield analysedGame
-    logging.warning(f'{playerId}: done analysing games')
 
 while True:
     logging.info('getting new job')
