2e30b4230c96fefcb9021b657c4b9d7a336d50b5
Lakin Wecker <lakin@structuredabstraction.com>
2018-10-01 11:15:47 -0600

    Improving logging when catching KeyErrors for apiCompleteJob

--- webapp/controllers/api/blueprint.py
+++ webapp/controllers/api/blueprint.py
@@ -7,6 +7,7 @@ from modules.irwin.PlayerReport import PlayerReport
 from modules.auth.Priv import RequestJob, CompleteJob, PostJob
 from modules.queue.Origin import OriginReport, OriginModerator, OriginRandom
 from modules.client.Job import Job
+import traceback
 
 def buildApiBlueprint(env):
     apiBlueprint = Blueprint('Api', __name__, url_prefix='/api')
@@ -43,19 +44,21 @@ def buildApiBlueprint(env):
             insertRes = env.gameApi.writeAnalysedGames(req['analysedGames'])
             if insertRes:
                 env.queue.completeEngineAnalysis(job.playerId)
-                
+
                 player = env.irwin.env.playerDB.byId(job.playerId)
                 analysedGames = env.irwin.env.analysedGameDB.byPlayerId(job.playerId)
                 games = env.irwin.env.gameDB.byIds([ag.gameId for ag in analysedGames])
                 predictions = env.irwin.analysedGameModel.predict([GameAnalysedGame(ag, g) for ag, g in zip(analysedGames, games) if ag.gameLength() <= 60])
-                
+
                 playerReport = PlayerReport.new(player, zip(analysedGames, predictions), owner = authable.name)
                 logging.warning(f'Sending player report for {playerReport.playerId}, activation {playerReport.activation}%')
                 env.lichessApi.postReport(playerReport)
 
                 return Success
-        except KeyError:
-            ...
+        except KeyError as e:
+            tb = traceback.format_exc()
+            logging.warning(f'Error completing job: {tb}')
+
         return BadRequest
 
     return apiBlueprint
